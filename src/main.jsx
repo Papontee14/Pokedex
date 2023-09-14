5490d0424d41c6c375d09569d10a8cafc707fb40
Papontee <108688651+Papontee14@users.noreply.github.com>
2023-09-14 23:30:16 +0700

    change main

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,19 @@ import PokemonDetail from './components/PokemonDetail.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: 'PokemonDetail',
-    element: <PokemonDetail />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+    },
+    {
+      path: 'PokemonDetail',
+      element: <PokemonDetail />,
+    },
+  ],
+  { basename: import.meta.env.DEV ? '/' : '/Pokedex/' }
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
